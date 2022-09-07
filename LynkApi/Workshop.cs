38fc8bc7abf20c372b985c5f62dd29dc3aaac778
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-09-07 11:34:26 +0200

    Liked textfile

--- LynkApi/Workshop.cs
+++ LynkApi/Workshop.cs
@@ -13,6 +13,5 @@ namespace LynkApi
         public string? BackOfficeWorkshopId { get; set; }
         public string? DisplayName { get; set; }
         public string? Time_Zone { get; set; } // lade till timezone
-
     }
 }
