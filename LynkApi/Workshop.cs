10eeaf17f5ae03caced6ae7a1f2800df6246de24
Maria <maria.sliufko@icloud.com>
2022-09-07 09:19:37 +0200

    workshopclass added

---
+++ LynkApi/Workshop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LynkApi
+{
+    public class Workshop
+    {
+        public int Id { get; set; } //guid ger ett unikt id
+        public int LocationId { get; set; }
+        public string? BackOfficeWorkshopId { get; set; }
+        public string? DisplayName { get; set; }
+
+    }
+}
