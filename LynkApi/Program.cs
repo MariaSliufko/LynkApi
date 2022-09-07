3cecc602d8dd3b0ee38dee7e2af3510de9e2a306
Maria <maria.sliufko@icloud.com>
2022-09-07 10:51:07 +0200

    div

--- LynkApi/Program.cs
+++ LynkApi/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
+using LynkApi;
+using System.Linq;
+using System.IO;
+
 Console.WriteLine("Hello, World!");
+
+List<string> lines = System.IO.File.ReadLines("C:\\Users\\masl\\source\\repos\\LynkApi\\LynkApi\\Workshopexamples.txt").ToList();
+Console.ReadLine();
