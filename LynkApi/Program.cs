c38643d8dc26bfdeb5d7ee0a4a1e1ebab449c80f
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-09-09 10:20:37 +0200

    Files

--- LynkApi/Program.cs
+++ LynkApi/Program.cs
@@ -1,29 +1,21 @@
-﻿
-// See https://aka.ms/new-console-template for more information
+﻿using static Microsoft.Graph.CoreConstants;
+using System.Net.Http.Headers;
+using System;
+using Azure;
+using System.Net.Http;
 
-String line;
-try
+using (var client = new HttpClient()) //Instansierar en ny HttpClient instans med syntaxen för objektinitieraren och som en användningsdeklaration.
 {
-    // kör filvägen 
-    StreamReader sr = new StreamReader("C:\\Users\\wiha\\source\\repos\\MariaSliufko\\LynkApi\\LynkApi\\Workshopexamples.txt");
-    //Läser texten
-    line = sr.ReadLine();
-    //Fortsätt till slutet
-    while (line != null)
-    {
-        Console.WriteLine(line);
-        //Läs nästa
-        line = sr.ReadLine();
-    }
-    //Stäng filen
-    sr.Close();
-    Console.ReadLine();
-}
-catch(Exception e)
-{
-    Console.WriteLine("Exception: " + e.Message);
-}
-finally
-{
-    Console.WriteLine("Executing finally block.");
+    //string key = "y2TpY8nt029M~OC3NdK7tXnpF";
+    //string key = Environment.GetEnvironmentVariable("API_KEY");
+    //var endpoint = new Uri("https://context-qa.lynkco.com/api/workshop/locations?api_key={key}"); //posts
+
+    var endpoint = new Uri("https://context-qa.lynkco.com/api/workshop/locations/"); //gör om alla api anrop till en separat klass
+    var request = new HttpRequestMessage(HttpMethod.Get, endpoint); //requestmesssage med endpoint
+    var token = "y2TpY8nt029M~OC3NdK7tXnpF";
+    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token); // en http har alltid en header, och den kan man fylla med värden, variabler med vad som helst, se headern som ett json objekt som man kan fylla med vad man vill
+    var result = client.SendAsync(request).Result; //skicka med http
+    Console.WriteLine(result);
+    var json = result.Content.ReadAsStringAsync().Result;
+    Console.WriteLine(json);
 }
