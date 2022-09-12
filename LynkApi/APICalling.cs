2f17a150fc797dd123f51d5871838c4636c6d01b
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-09-12 13:56:53 +0200

    added api in a class and menu

---
+++ LynkApi/APICalling.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using LynkApi;
+using Newtonsoft.Json;
+
+namespace LynkApi
+{
+    public class GetByAPI
+    {
+        public static async Task<GetByAPI> Calling()
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri("https://context-qa.lynkco.com/api/workshop/locations/");
+
+                try
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, endpoint); //requestmesssage med endpoint 
+                    var token = "y2TpY8nt029M~OC3NdK7tXnpF";
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    var result = client.SendAsync(request).Result;
+                    string json = result.Content.ReadAsStringAsync().Result;
+
+
+                    GetByAPI getByAPI = JsonConvert.DeserializeObject<GetByAPI>(json);
+
+                    return getByAPI;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine("\nPress any key to return to menu");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                    Startmenu.Menu();
+                    return null;
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
--- LynkApi/Program.cs
+++ LynkApi/Program.cs
@@ -1,63 +1,18 @@
 using System.Net.Http.Headers;
 using System.Net.Http;
 using Newtonsoft.Json;
-using LynkProject;
+using LynkApi;
+using System.IO;
 
 
-
-//main ska vara static async Task Main(string[] args)?
-
-
-using (var client = new HttpClient())
-//Instansierar en ny HttpClient instans med syntaxen för objektinitieraren och som en användningsdeklaration.
-
+namespace LynkApi
 {
-    //string key = "y2TpY8nt029M~OC3NdK7tXnpF";
-    //string key = Environment.GetEnvironmentVariable("API_KEY");
-    //var endpoint = new Uri("https://context-qa.lynkco.com/api/workshop/locations?api_key={key}"); //posts
-
-
-    var endpoint = new Uri("https://context-qa.lynkco.com/api/workshop/locations/"); // gör om alla api anrop till en separat klass
-
-    try
+    class Program
     {
-        var request = new HttpRequestMessage(HttpMethod.Get, endpoint); //requestmesssage med endpoint //Ska vara await?
-        var token = "y2TpY8nt029M~OC3NdK7tXnpF";
-        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token); // en http har alltid en header, och den kan man fylla med värden, variabler med vad som helst, se headern som ett json objekt som man kan fylla med vad man vill
-
-        var result = client.SendAsync(request).Result; //Ska vara await?
-        Console.WriteLine(result);
-        Console.ReadLine();
-
-        var json = result.Content.ReadAsStringAsync().Result;
-        Console.WriteLine(json);
-
-        //Deserialize json response till c# array av typen Post[]
-        var myWorkshops = JsonConvert.DeserializeObject<Workshop[]>(json);
-        //skriv ut array med objecten med foreach loop
-        foreach (var workshop in myWorkshops)
+        static void Main(string[] args)
         {
-            Console.WriteLine($"[{workshop.LocationId} {workshop.BackOfficeWorkshopId} {workshop.DisplayName} {workshop.TimeZone}");
-            //Console.ReadLine();
+            Startmenu start = new Startmenu();//Kallar på metoden för start meny.
+            start.Start();//Kallar på metoden för att starta menyn.
         }
-
-
     }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-
-    }
-    //finally 
-    //{
-    //    //disposa httpClient
-    //    httpClient.Dispose();
-    //}
-
-
-
-
-
 }
-
-
