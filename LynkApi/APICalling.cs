1c6a1c19f6551cd9339f80ed04cc0b47318ca46e
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-09-12 14:53:09 +0200

    added api class

--- LynkApi/APICalling.cs
+++ LynkApi/APICalling.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿
+using LynkApi;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LynkApi
 {
-    internal class APICalling
+    public class GetByAPI
     {
+        public static async Task<GetByAPI> Calling()
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri("https://context-qa.lynkco.com/api/workshop/locations/");
+
+                try
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Get, endpoint); //requestmesssage med endpoint 
+                    var token = "y2TpY8nt029M~OC3NdK7tXnpF";
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    var result = client.SendAsync(request).Result;
+                    string json = result.Content.ReadAsStringAsync().Result;
+
+
+                    GetByAPI getByAPI = JsonConvert.DeserializeObject<GetByAPI>(json);
+
+                    return getByAPI;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine("\nPress any key to return to menu");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                    Startmenu.Menu();
+                    return null;
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+            }
+        }
     }
 }
