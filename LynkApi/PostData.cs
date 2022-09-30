3821370cfaa1191a6016d459c9418965cafc0876
Maria <maria.sliufko@icloud.com>
2022-09-30 13:44:20 +0200

    GetAppointmentsApiClientWorking

---
+++ LynkApi/PostData.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Security.Claims;
+//using System.Text;
+//using System.Threading.Tasks;
+//using LynkApi;
+
+//namespace LynkApi
+//{
+//    public class PostData
+//    {
+
+//        public static string Something()
+//        { 
+//            HttpClient client = new HttpClient();
+//            HttpClient.baseAddress = new Uri("https://context-qa.lynkco.com/api/workshop/");
+        
+        
+        
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    }
+//}
