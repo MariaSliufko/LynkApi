5db576f3bab4690ce040727ce2e49a3930897956
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-10-14 13:50:25 +0200

    fixed menu

---
+++ LynkApi/SearchVehicle.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestLynk;
+
+namespace LynkApi
+{
+    public class SearchVehicle
+    {
+        static string dataDirectyory = Path.Combine(Path.GetDirectoryName((typeof(Program).Assembly.Location)) ?? string.Empty, "data"); // skapar filvägen
+
+        public static void getVehicle()
+        {
+
+            Console.WriteLine("Type Vehicle ID:");
+            string vId = Console.ReadLine();
+
+            var dir = dataDirectyory + "/vehicles/" + vId + ".json";
+            // variablen dir = är data patcchen + viachles mappen +  Viachle id med matchade id + .json
+            // aka sökvägen till filen som vi vill läsa in
+
+            if (File.Exists(dir)) // om idt finns 
+            {
+                using (StreamReader sr = new StreamReader(dir)) // då läser den raden som finns i filen då vi vet att all data står på 1 rad 
+                {
+                    var line = sr.ReadLine();
+                    var obj = JObject.Parse(line); //läser in denna linen som ett json objekt
+                    Console.WriteLine(obj); // skruver ut det snyggt pga jobjekt classen
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vehicle ID not found");
+            }
+        }
+    }
+}
--- LynkApi/Startmenu.cs
+++ LynkApi/Startmenu.cs
@@ -13,9 +13,6 @@ namespace LynkApi
 {
     public class Startmenu
     {
-
-        static string dataDirectyory = Path.Combine(Path.GetDirectoryName((typeof(Program).Assembly.Location)) ?? string.Empty, "data"); // skapar filvägen
-
         public static void Menu()//Metod
         {
             int option = 0;//Börja med värde 0
@@ -51,7 +48,7 @@ namespace LynkApi
                     SearchWorkshop.getWorkshop();
                     break;
                 case 3:
-                    getVehicle(); // exakt samma kod, som Appointments 
+                    SearchVehicle.getVehicle(); // exakt samma kod, som Appointments 
                    
                     break;
                 case 4:
@@ -68,32 +65,6 @@ namespace LynkApi
                     Console.Clear();
                     Menu();
                     break;
-            }
-        }
-
-
-        public static void getVehicle()
-        {
-
-            Console.WriteLine("Type Vehicle ID:");
-            string vId = Console.ReadLine();
-
-            var dir = dataDirectyory + "/vehicles/" + vId + ".json";
-            // variablen dir = är data patcchen + viachles mappen +  Viachle id med matchade id + .json
-            // aka sökvägen till filen som vi vill läsa in
-
-            if (File.Exists(dir)) // om idt finns 
-            {
-                using (StreamReader sr = new StreamReader(dir)) // då läser den raden som finns i filen då vi vet att all data står på 1 rad 
-                {
-                    var line = sr.ReadLine();
-                    var obj = JObject.Parse(line); //läser in denna linen som ett json objekt
-                    Console.WriteLine(obj); // skruver ut det snyggt pga jobjekt classen
-                }
-            }
-            else
-            {
-                Console.WriteLine("Vehicle ID not found");
             }
         }
         public void Start()//Metod för att starta metod Menu
