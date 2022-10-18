a0ef9a7d9fb7bb1af40242581a4259d8fcf86745
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-10-18 23:21:35 +0200

    funkar

--- LynkApi/VehicleSummery.cs
+++ LynkApi/VehicleSummery.cs
@@ -11,22 +11,13 @@ namespace LynkApi
 {
     public class VehicleSummery // Lägg till vehicle summery. Räkna ihop totalt antal vehicles.
     {
-        static string dataDirectory = Path.Combine(Path.GetDirectoryName((typeof(Program).Assembly.Location)) ?? string.Empty, "data/workshops");
+        static string dataDirectory = Path.Combine(Path.GetDirectoryName((typeof(Program).Assembly.Location)) ?? string.Empty, "data/vehicles");
 
         public static void Sort()
         {
             var files = Directory.GetFiles(dataDirectory); // Get all the files from the directory
-            // LÄGG OrderBy på Files
 
-            //Console.WriteLine("Showing info about " + files. + " total appointments:"); //skriver ut totala antl AP
-
-            foreach (var file in files)
-            {
-                string jsonString = File.ReadAllText(file); // Read the text from the file
-
-                var obj = JsonConvert.DeserializeObject<WorkshopModel>(jsonString); // Convert the file into WorkshopModel
-                Console.WriteLine("* " +obj.DisplayName + " "+ obj.Vehicles.Count()); ////CW the Obj display name and Count of Cars
-            }
+            Console.WriteLine("Total number of Vehicles: " + files.Count()); // Print length of files
         }
     }
 }
