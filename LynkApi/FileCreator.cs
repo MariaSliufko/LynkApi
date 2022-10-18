c73d79d03c753754ad540d1c87ca4e25fc300cdb
Wilma Hammarshall <wilmahammarshall@gmail.com>
2022-10-19 00:16:19 +0200

    test

--- LynkApi/FileCreator.cs
+++ LynkApi/FileCreator.cs
@@ -18,36 +18,43 @@ namespace LynkApi
 
         // C:\Users\masl\source\repos\LynkApi\LynkApi\bin\Debug\net6.0\data
         static string dataDirectyory = Path.Combine(Path.GetDirectoryName((typeof(Program).Assembly.Location)) ?? string.Empty, "data"); // skapar filvägen C:/documents/maria/github/lykapi/bin + /data
-        static string dataFile = Path.Combine(dataDirectyory, "allAppointmentData.json"); // skapar filen
-        // GÖR DENNA ASYNC MED AWAIT 
-        public static void FetchDataFromDatabase()
+                                                                                          // GÖR DENNA ASYNC MED AWAIT 
+        static string baseadress = "https://context-qa.lynkco.com/api/workshop/";
+        static string apiToken = "y2TpY8nt029M~OC3NdK7tXnpF"; // "API_TOKEN"; Not able to use API_TOKEN to replace api token
+
+        static ApiClient api = new ApiClient(new Uri(baseadress), apiToken);
+        static List<AppointmentModel> appointmentList = new List<AppointmentModel>(); // skapar en appointmentList
+        static List<VehicleModel> vehicleList = new List<VehicleModel>(); // skapar en vehicleList
+
+        public static async void FetchDataFromDatabase()
         {
 
-            Console.WriteLine("Confirm if you want to download and uppdate all data by typing Y: ");
+           // Console.WriteLine("Confirm if you want to download and uppdate all data by typing Y: ");
             
-            string key = Console.ReadLine().ToUpper();
+           // string key = Console.ReadLine().ToUpper();
 
-            if (key == "Y")
+            if ( true) //key == "Y")
             {
 
                 Console.WriteLine("Progress bar console simulation");
 
                 var progressBarSimulator = new ConsoleLineUpdater(units: 100, steps: 50, milliSec: 5000); //ändra fasta parametrar
-                progressBarSimulator.RunSimulation();
+               // progressBarSimulator.RunSimulation();
 
-                var baseadress = "https://context-qa.lynkco.com/api/workshop/";
-                var apiToken = "y2TpY8nt029M~OC3NdK7tXnpF"; // "API_TOKEN"; Not able to use API_TOKEN to replace api token
-
-                ApiClient api = new ApiClient(new Uri(baseadress), apiToken);
+               
+               
                 api = new ApiClient(new Uri(baseadress), apiToken);
 
-                var result = api.GetWorkshops().Result; // hämtar alla WS
-                List<AppointmentModel> appointmentList = new List<AppointmentModel>(); // skapar en appointmentList
-                List<VehicleModel> vehicleList = new List<VehicleModel>(); // skapar en vehicleList
+                var workshops = api.GetWorkshops().Result; // hämtar alla WS
 
 
                 // tanken är sen att vi ska lagra alla AP och alla V i dessa listor, kmr bli många rader långa 
 
+                if (!Directory.Exists(dataDirectyory)) // om directoryn inte finns för workshops
+                {
+                    Console.WriteLine("No folders detected. \n Creating data directory");
+                    Directory.CreateDirectory(dataDirectyory); // så sklapar den mappen för Workshops
+                }
 
                 if (!Directory.Exists(dataDirectyory + "/workshops")) // om directoryn inte finns för workshops
                 {
@@ -65,39 +72,33 @@ namespace LynkApi
                     Directory.CreateDirectory(dataDirectyory + "/vehicles"); // så skapar den en map för vehicles
                 }
 
-                foreach (var workshop in result) // när vi hittar alla WS går vi igenom varje WS, först skapar vi alla mappar se ovan om de ej finns. 
-                {
-
-                    var appointments = api.GetAppointments(workshop.LocationId).Result;   // hämtar alla AP från APIT med location id 
-                    appointmentList.AddRange(appointments); // lägger till alla AP i listan, rage lägger till flera saker i listan ex om de kmr 100 st nya AP läggs det till i listan som vi skapade först
-                    workshop.Appointments = appointments.Select(a => a.AppointmentId).ToList(); //lamda uttryck, i själva WS:s sträng lista så lägger vi till varje AP (för varje a i appointment så tar vi AP id bara och så gör vi det till en lista 
-                                                                                                // denna raden skapar stränglistan med alla Ap ids, så om en AP har idt 23 och ligger på WS med id 1 så kmr den filen se ut såhär 1.23 . dvs wsid+APid blir namnet på filen
 
 
-                    var vehicles = api.GetVehicles(workshop.LocationId).Result; // samma på V, vi hämtar alla V för den WS 
-                    vehicleList.AddRange(vehicles); // lägger till varje V i den 1a listan vi skapade 
-                    workshop.Vehicles = vehicles.Select(v => v.VehicleId).ToList(); // sen lägger vi till varje V id i WS som sträng, den tar v:t för vehicle och hämtar idt och gör det till en lista 
+                Console.WriteLine("Fetching data START");
 
-                    var file = dataDirectyory + "/workshops/" + workshop.LocationId + ".json"; // vi skapar mappen WS, i denna sakapr vi filen workshop.LocationId.json så det vblir typ WS 1 etc som speglar idt på alla WS
-                    File.WriteAllText(file, JsonConvert.SerializeObject(workshop)); // ¨till den filen vi skapade ovan lägger vi till och serialisar objektet gör om den modellen vi har till JSON, det är drf vi får det utskrivet och formaterat finare
+                var tasks1 = workshops.Select(w => createWorkshopFiles(w));
 
-                    // så för varje WS hämtar vi alla AP och lägger till i listan och hämtar alla V och lägger till i listan 
-                }
+                Task.WhenAll(tasks1).Wait();
 
-                foreach (var appointment in appointmentList) // när vi gått igenom alla WS så går vi igenom alla AP i den listan, så alla AP vi gått igenom och lagt till idn på då går vi igenom varje objekt
-                {
+                Console.WriteLine("Fetching data DONE");
 
-                    var file = dataDirectyory + "/appointments/" + appointment.AppointmentId + ".json"; // för varje AP lägger vi i AP mappen och AP id samma som ovan med idt och namnet som rad 58
-                    File.WriteAllText(file, JsonConvert.SerializeObject(appointment));
-                }
+                Console.WriteLine("Creating appointments START");
 
-                foreach (var vehicle in vehicleList)
-                {
+                var tasks2 = appointmentList.Select(a => createAppointmentFiles(a));
 
-                    var file = dataDirectyory + "/vehicles/" + vehicle.VehicleId + ".json";
+                Task.WhenAll(tasks2).Wait();
+                Console.WriteLine("Creating appointments DONE");
 
-                    File.WriteAllText(file, JsonConvert.SerializeObject(vehicle)); // serialiserar objekten till json
-                }
+                Console.WriteLine("Creating vehicles START");
+
+                var tasks3 = vehicleList.Select(v => createVehicleFiles(v));
+
+                Task.WhenAll(tasks3).Wait();
+                Console.WriteLine("Creating vehicles DONE");
+
+
+
+               
             }
             else
             {
@@ -106,6 +107,56 @@ namespace LynkApi
 
             Console.WriteLine("\nThe simulation completed successfully.");
             Console.ReadKey();
+        }
+
+        private static async Task createVehicleFiles(VehicleModel vehicle)
+        {
+            await Task.Run(() =>
+            {
+
+               
+                var file = dataDirectyory + "/vehicles/" + vehicle.VehicleId + ".json";
+
+                File.WriteAllText(file, JsonConvert.SerializeObject(vehicle)); // serialiserar objekten till json
+
+               
+            });
+
+        }
+
+        private static async Task createAppointmentFiles(AppointmentModel appointment)
+        {
+                await Task.Run(() =>
+                {
+
+                    var file = dataDirectyory + "/appointments/" + appointment.AppointmentId + ".json"; // för varje AP lägger vi i AP mappen och AP id samma som ovan med idt och namnet som rad 58
+                    File.WriteAllText(file, JsonConvert.SerializeObject(appointment));
+                });
+        }
+
+        private static async Task createWorkshopFiles(WorkshopModel workshop)
+        {
+
+            await Task.Run(() =>
+            {
+                
+
+                var appointments = api.GetAppointments(workshop.LocationId).Result;   // hämtar alla AP från APIT med location id 
+                appointmentList.AddRange(appointments); // lägger till alla AP i listan, rage lägger till flera saker i listan ex om de kmr 100 st nya AP läggs det till i listan som vi skapade först
+                workshop.Appointments = appointments.Select(a => a.AppointmentId).ToList(); //lamda uttryck, i själva WS:s sträng lista så lägger vi till varje AP (för varje a i appointment så tar vi AP id bara och så gör vi det till en lista 
+                                                                                            // denna raden skapar stränglistan med alla Ap ids, så om en AP har idt 23 och ligger på WS med id 1 så kmr den filen se ut såhär 1.23 . dvs wsid+APid blir namnet på filen
+
+                var vehicles = api.GetVehicles(workshop.LocationId).Result; // samma på V, vi hämtar alla V för den WS 
+                vehicleList.AddRange(vehicles); // lägger till varje V i den 1a listan vi skapade 
+                workshop.Vehicles = vehicles.Select(v => v.VehicleId).ToList(); // sen lägger vi till varje V id i WS som sträng, den tar v:t för vehicle och hämtar idt och gör det till en lista 
+
+                
+                var file = dataDirectyory + "/workshops/" + workshop.LocationId + ".json"; // vi skapar mappen WS, i denna sakapr vi filen workshop.LocationId.json så det vblir typ WS 1 etc som speglar idt på alla WS
+                File.WriteAllText(file, JsonConvert.SerializeObject(workshop)); // ¨till den filen vi skapade ovan lägger vi till och serialisar objektet gör om den modellen vi har till JSON, det är drf vi får det utskrivet och formaterat finare
+               
+            });
+
+
         }
     }
 }
